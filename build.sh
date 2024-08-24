54ba650c660ee4940d6fe88b17e3113b5917084c
mehul gautam <mehulsharma4786@gamil.com>
2024-08-24 16:32:40 +0000

    added build script
    Signed-off-by: mehul gautam <mehulsharma4786@gamil.com>

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+CROSS_COMPILE=/home/kali/opt/cross/bin/i686-elf-
+"${CROSS_COMPILE}"as boot.s -o boot.o
+"${CROSS_COMPILE}"gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+"${CROSS_COMPILE}"gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
